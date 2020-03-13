b2d830e8008743276ea7a55941beb327d48a3d5b
wakennedy <alexkenned@gmail.com>
2020-03-13 08:57:07 -0700

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Customer
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+          meal.customer == self
+        end
+    end
+
+    def waiters
+        self.meals.map do |meal|
+            meal.waiter
+        end
+
+    end
+
   
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,39 @@
 class Waiter
+    attr_accessor :name, :yoe
+
+    @@all = []
+
+    def initialize(name, yoe)
+        @name = name
+        @yoe = yoe
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+          meal.waiter == self
+        end
+    end
+
+    def best_tipper
+        i = 0 
+        bt = nil
+        Meal.all.each do |meal|
+            if meal.tip > i
+                i = meal.tip
+                bt = meal.customer
+            end
+        end
+        bt
+    end
+
 
 end
